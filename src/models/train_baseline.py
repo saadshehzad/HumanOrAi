e700654d31c6324d966789d9a8c9d9cc252c6a84
saadshehzad <xaadxhehzad@gmail.com>
2026-09-24 17:44:08 -0400

    Remove prints

--- src/models/train_baseline.py
+++ src/models/train_baseline.py
@@ -7,9 +7,6 @@ from sklearn.metrics import accuracy_score, classification_report
 train = load_from_disk("src/data/processed/hc3/train")
 validation = load_from_disk("src/data/processed/hc3/validation")
 
-print("Train:", len(train))
-print("Validation:", len(validation))
-
 
 X_train = train["text"]
 y_train = train["label"]
@@ -25,8 +22,6 @@ vectorizer = TfidfVectorizer(
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_validation_tfidf = vectorizer.transform(X_validation)
 
-print("Train TF-IDF shape:", X_train_tfidf.shape)
-print("Validation TF-IDF shape:", X_validation_tfidf.shape)
 
 
 model = LogisticRegression(max_iter=1000)
@@ -35,17 +30,11 @@ model.fit(X_train_tfidf, y_train)
 
 validation_predictions = model.predict(X_validation_tfidf)
 
-print("Validation predictions:", validation_predictions[:10])
-
 
 accuracy = accuracy_score(
     y_validation,
     validation_predictions
 )
-
-print("Validation Accuracy:", accuracy)
-
-print("\nClassification Report:")
 
 print(
     classification_report(
