eac31da4f13feb9f01ce298e7e9d0ea617f0c97b
saadshehzad <xaadxhehzad@gmail.com>
2026-09-24 17:40:31 -0400

    Predict and check ccuracy

--- src/models/train_baseline.py
+++ src/models/train_baseline.py
@@ -1,5 +1,7 @@
 from datasets import load_from_disk
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 
 train = load_from_disk("src/data/processed/hc3/train")
@@ -23,6 +25,32 @@ vectorizer = TfidfVectorizer(
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_validation_tfidf = vectorizer.transform(X_validation)
 
-
 print("Train TF-IDF shape:", X_train_tfidf.shape)
 print("Validation TF-IDF shape:", X_validation_tfidf.shape)
+
+
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train_tfidf, y_train)
+
+validation_predictions = model.predict(X_validation_tfidf)
+
+print("Validation predictions:", validation_predictions[:10])
+
+
+accuracy = accuracy_score(
+    y_validation,
+    validation_predictions
+)
+
+print("Validation Accuracy:", accuracy)
+
+print("\nClassification Report:")
+
+print(
+    classification_report(
+        y_validation,
+        validation_predictions,
+        target_names=["Human", "AI"]
+    )
+)
